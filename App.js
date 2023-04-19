1bee07e606bc07dd8aeba39575c4a47947d93656
Matheus Alexandre Bezerra <111669430+MathAlexan@users.noreply.github.com>
2023-04-19 11:03:48 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,127 +1,28 @@
-import React, { useState } from "react";
-import Ionicons from "@expo/vector-icons/Ionicons";
-import { AntDesign } from '@expo/vector-icons';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, Alert } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, TextInputComponent, View } from 'react-native';
 
-const App = () => {
-  const onPressAcessar = () => {
-    // Ao clicar em Acessar faz algo
-    Alert.alert('Acesso liberado!', `${nome},${cpf}`)
-  };
-  const onPressEsqueceuASenha = () => {
-    // Ao clicar em Esqueceu a senha faz algo
-  };
-  const onPressCadastrar = () => {
-    // faz algo ao clicar para se cadastrar
-  };
-  const [nome, setNome] = useState("");
-  const [cpf, setCpf] = useState("");
-
+export default function App() {
   return (
     <View style={styles.container}>
-
-     <View style={styles.box1}>
-      <Text style={styles.titulo}>Kalima's Restaurant</Text>
-     </View> 
-
-      <View style={styles.inputView}>
-        <Ionicons name="person" size={24} color="black" />
-        <TextInput
-          style={styles.inputText}
-          placeholder=" Nome Completo"
-          placeholderTextColor="#003f5c"
-          onChangeText={(text) => setEmail(text)}/>
-      </View>
-
-      <View style={styles.inputView}>
-      <AntDesign name="idcard" size={24} color="black" />
-        <TextInput
-          style={styles.inputText}
-          secureTextEntry
-          placeholder=" CPF "
-          placeholderTextColor="#003f5c"
-          onChangeText={(text) => setPassword(text)}
-        />
-      </View>
-
-      <TouchableOpacity onPress={onPressEsqueceuASenha}>
-        <Text style={styles.forgotAndSignUpText}>Esqueceu a senha?</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity onPress={onPressAcessar} style={styles.loginButton}>
-        <Text style={styles.loginText}>Acessar</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity onPress={onPressCadastrar}>
-        <Text style={styles.forgotAndSignUpText}>Criar Conta</Text>
-      </TouchableOpacity>
+      <Text style={styles.font}>Digite seu nome:</Text>
+      <TextInput style = {styles.inputText} />
+      <StatusBar style="auto" />
     </View>
   );
-};
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 25,
-    backgroundColor: "#FF1493",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#0e87fa',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  
-  box1: {
-    marginBottom: 30,
+  font:{
+    fontSize : 20,
+    fontWeight:"bold",
   },
-
-  titulo: {
-    fontWeight: "bold",
-    color: "#8B008B",
-    textShadowColor: "black",
-    fontSize: 40,
-    textShadowRadius: 1,
-    textShadowOffset: { width: 2, height: 2 },
-  },
-
-  inputView: {
-    fontWeight: "bold",
-    backgroundColor: "white",
-    borderColor: "black",
-    alignItems: "center",
-    flexDirection: "row",
-    width: 350,
-    height: 75,
-    marginBottom: 20,
-    padding: 20,
-    borderRadius: 10,
-    borderWidth: 1,
-  },
-  
-  inputText: {
-    height: 50,
-    color: "black",
-  },
-
-  loginText: {
-    fontWeight: "bold",
-    flexDirection: "row",
-    color:'black',
-    fontSize: 16,
-  },
-
-  forgotAndSignUpText: {
-    color: 'black',
-    fontSize: 14,
-  },
-
-  loginButton: {
-    backgroundColor: '#DDA0DD',
-    borderColor: "black",
-    alignItems: "center",
-    justifyContent: "center",
-    width: 200,
-    height: 50,
-    marginTop: 40,
-    marginBottom: 10,
-    borderRadius: 10,
-    borderWidth: 1,
-  },
+  inputText:{
+    padding: 5
+  }
 });
-export default App;
